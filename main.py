251c5d3d05c92e0b4446ba9c577b23761861190f
Artyom <artem_kim_2006@list.ru>
2023-04-30 12:36:20 +0600

    added collision

--- main.py
+++ main.py
@@ -22,6 +22,131 @@ def draw_grid():
 		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
 		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
+class Player():
+	def __init__(self, x, y):
+		img = pygame.image.load('img/Fireboy.webp')
+		self.image = pygame.transform.scale(img, (40, 80))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.vel_y = 0
+		self.jumped = False
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
+
+	def update(self):
+		dx = 0
+		dy = 0
+
+		#get keypresses
+		key = pygame.key.get_pressed()
+		if key[pygame.K_UP] and self.jumped == False:
+			self.vel_y = -15
+			self.jumped = True
+		if key[pygame.K_UP] == False:
+			self.jumped = False
+		if key[pygame.K_LEFT]:
+			dx -= 5
+		if key[pygame.K_RIGHT]:
+			dx += 5
+
+
+		#add gravity
+		self.vel_y += 1
+		if self.vel_y > 10:
+			self.vel_y = 10
+		dy += self.vel_y
+
+		#check for collision
+		for tile in world.tile_list:
+			#check for collision in x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground i.e. jumping
+				if self.vel_y < 0:
+					dy = tile[1].bottom - self.rect.top
+					self.vel_y = 0
+				#check if above the ground i.e. falling
+				elif self.vel_y >= 0:
+					dy = tile[1].top - self.rect.bottom
+					self.vel_y = 0
+
+		#update player coordinates
+		self.rect.x += dx
+		self.rect.y += dy
+
+		if self.rect.bottom > screen_height:
+			self.rect.bottom = screen_height
+			dy = 0
+
+		#draw player onto screen
+		screen.blit(self.image, self.rect)
+		pygame.draw.rect(screen, (255, 255, 255), self.rect, 2)
+
+class Player2():
+	def __init__(self, x, y):
+		img = pygame.image.load('img/Watergirl.webp')
+		self.image = pygame.transform.scale(img, (40, 120))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+        
+		self.vel_y = 0
+		self.jumped = False
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
+
+	def update(self):
+		dx = 0
+		dy = 0
+
+		#get keypresses
+		key = pygame.key.get_pressed()
+		if key[pygame.K_w] and self.jumped == False:
+			self.vel_y = -15
+			self.jumped = True
+		if key[pygame.K_w] == False:
+			self.jumped = False
+		if key[pygame.K_a]:
+			dx -= 5
+		if key[pygame.K_d]:
+			dx += 5
+
+
+		#add gravity
+		self.vel_y += 1
+		if self.vel_y > 10:
+			self.vel_y = 10
+		dy += self.vel_y
+
+		#check for collision
+		for tile in world.tile_list:
+			#check for collision in x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground i.e. jumping
+				if self.vel_y < 0:
+					dy = tile[1].bottom - self.rect.top
+					self.vel_y = 0
+				#check if above the ground i.e. falling
+				elif self.vel_y >= 0:
+					dy = tile[1].top - self.rect.bottom
+					self.vel_y = 0
+				
+		#update player coordinates
+		self.rect.x += dx
+		self.rect.y += dy
+
+		if self.rect.bottom > screen_height:
+			self.rect.bottom = screen_height
+			dy = 0
+
+		#draw player onto screen
+		screen.blit(self.image, self.rect)
 
 
 class World():
@@ -56,11 +181,12 @@ class World():
 	def draw(self):
 		for tile in self.tile_list:
 			screen.blit(tile[0], tile[1])
+			pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
 
 
 
 world_data = [
-[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
 [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
@@ -84,16 +210,22 @@ world_data = [
 
 
 
-
+player = Player(100, screen_height - 130)
+player2 = Player2(100, screen_height - 130)
 world = World(world_data)
+
 
 run = True
 while run:
 
 	screen.blit(bg_img, (0, 0))
 	screen.blit(sun_img, (100, 100))
-
+	
+   
 	world.draw()
+	player2.update()
+	player.update()
+
 
 	draw_grid()
 
